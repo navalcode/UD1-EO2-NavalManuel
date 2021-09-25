7a97c609ca409de725073e1816e12c0b56109381
Manuel Naval <navalrod@gmail.com>
2021-09-25 08:52:22 +0200

    se agrega la plantilla del proyecto

---
+++ EO2/src/main/java/com/salesianos/triana/EO2/Eo2Application.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.EO2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eo2Application.class, args);
+	}
+
+}
---
+++ EO2/src/test/java/com/salesianos/triana/EO2/Eo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.EO2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
